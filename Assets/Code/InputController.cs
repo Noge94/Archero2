c57158414c9ae32a730bfef36c5679878292d380
Noge Vallès <nogeh94@gmail.com>
2019-09-08 20:52:41 +0200

    Player basic movement

---
+++ Assets/Code/InputController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+    public class InputController : MonoBehaviour
+    {
+        public static InputController instance;
+        public Vector3 stick = Vector3.zero;
+
+        private bool attacking = false;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(instance);
+            }
+        }
+        
+        void Update()
+        {
+            stick = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                stick.y += 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                stick.y -= 1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                stick.x += 1;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                stick.x -= 1;
+            }
+
+            if (stick.magnitude != 0)
+            {
+                StopAttacking();
+                stick = stick / stick.magnitude;
+            }
+            else
+            {
+                StartAttacking();
+            }
+        }
+
+        private void StopAttacking()
+        {
+            if (!attacking)
+            {
+                return;
+            }
+            else
+            {
+                attacking = false;
+                Debug.Log("Stop attacking uwu");
+            }
+        }
+
+        private void StartAttacking()
+        {
+            if (attacking)
+            {
+                return;
+            }
+            else
+            {
+                attacking = true;
+                Debug.Log("I attack now hehehe");
+            }
+        }
+    }
+}
--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,9 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Code;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    
+    public static Player instance;
+    public float speed;
 
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
+    }
     
-    
+    private void Update()
+    {
+
+        Vector3 movement = InputController.instance.stick * speed * Time.deltaTime;
+
+        transform.position = transform.position + movement;
+
+    }
 }
